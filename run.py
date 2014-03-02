fc3b5eceade648942ecc197148bce55daa6c4125
Chi Wang <chiwang@mail.ustc.edu.cn>
2014-03-02 17:19:46 +0800

    modified:   run.py

--- run.py
+++ run.py
@@ -7,11 +7,11 @@ if os.environ['DMPSWSYS'] not in sys.path:
     sys.path.append(os.getenv('DMPSWSYS'))
     import libpythonmain
 '''
-import libpythonmain
+import libpythonmain as DmpSimu
 
-libpythonmain.helloworld("DAMPE")
-libpythonmain.test()
+DmpSimu.helloworld("DAMPE")
+DmpSimu.test()
 
-libpythonmain.setN(7)
-libpythonmain.helloworld("new run")
-libpythonmain.test()
+DmpSimu.setN(7)
+DmpSimu.helloworld("new run")
+DmpSimu.test()
